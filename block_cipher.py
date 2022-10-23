8802b68980cba2e64369d57c9e7842a5a76751c8
chinweibegbu <chinwe.ibegbu@gmail.com>
2022-10-23 12:09:17 +0000

    Completed tasks 1 and 2

--- block_cipher.py
+++ block_cipher.py
@@ -1,11 +1,19 @@
-import sys
-import os
+from pydoc import plain
+import sys, os
+import urllib.parse as url_encode
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from numpy import byte
+from numpy import byte, full, true_divide
 
-def prepFile():
-    pass
+ # Create key and initialisation vector of size 128 bits
+key = os.urandom(16)
+iv = os.urandom(16)
+
+# AES Decryptor and Encryptor
+AES_algorithm = algorithms.AES128(key)
+cipher = Cipher(AES_algorithm, mode=modes.CBC(iv))
+encryptor = cipher.encryptor()
+decryptor = cipher.decryptor()
 
 def encrypt_ecb():
     key = os.urandom(16)
@@ -71,13 +79,7 @@ def encrypt_ecb():
     return 0
 
 
-
-
-
 def encrypt_cbc():
-        # Create key and initialisation vector of size 128 bits
-    key = os.urandom(16)
-    iv = os.urandom(16)
 
     # Read the content of the plaintext file
     plaintext_file_name = sys.argv[1]
@@ -149,6 +151,66 @@ def encrypt_cbc():
     # plaintext_file.close()
     return 1
 
-encrypt_ecb()
+# encrypt_ecb()
 
 
+def submit():
+
+    # Getting the user input and making it an array
+    userInput = input("Please give me a string: \n")
+    letters = [*userInput]
+
+    # Encoding each ';' and '=' in the userInput
+    for i in range(len(letters)):
+        if letters[i] == ';' or letters[i] == '=':
+            urlEncoded = url_encode.quote(letters[i])
+            letters[i] = urlEncoded
+
+    # Putting the characters back together into one variable
+    encodedUserInput = ""
+    for i in letters:
+        encodedUserInput += i
+
+    # Adding the appropriate prepend and appent to the encoded userInput
+    prepend = "userid=456;userdata="
+    append = ";session-id=31337" 
+    full_text = prepend + encodedUserInput  + append;
+
+    # Printing the output of the full_text
+    print("Full configured text: ", full_text, "\n") 
+
+    # Converting full_text to bytes
+    textInBytes = str.encode(full_text)
+
+    # Padding the full_text
+    padder = padding.PKCS7(128).padder()
+    paddedText = padder.update(textInBytes) + padder.finalize()
+    
+    # Printing the padded text
+    print("Padded text: ", paddedText, "\n")
+
+    # Encrypting padded text to AES-128-CBC
+    cipher_text = encryptor.update(paddedText) + encryptor.finalize()
+
+    # Printing the cipher_text in bytes...
+    print("AES Encrypted cipher text is: \n", cipher_text, "\n" )
+
+    return(cipher_text)
+
+def verify(cipher_text):
+    plain_text = decryptor.update(cipher_text) + decryptor.finalize()
+    plain_text = plain_text.decode("utf-8") 
+    print("The plain text is: \n", plain_text)
+    index = plain_text.find(";admin=true;")
+    if index > -1:
+        return True
+    else:
+        return False
+    # if ";admin=true;" in plain_text:
+    #     return True
+    # else:
+    #     return False
+     
+ct = submit()
+result = verify(ct)
+print("\nOutcome:", result)
